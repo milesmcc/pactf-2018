85be0fb73de5e94a9986c0d73f8f090e408dec76
Miles McCain <milesmcc@users.noreply.github.com>
2018-03-06 10:33:26 -0500

    update email settings

--- django/pactf/settings.py
+++ django/pactf/settings.py
@@ -182,8 +182,8 @@ class _Django(Configuration):
     EMAIL_HOST_USER = values.Value('apikey', environ_prefix=None)
     EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix=None)
 
-    DEFAULT_FROM_EMAIL = values.Value('contact@pactf.com', environ_prefix=email_prefix)
-    SERVER_EMAIL = values.Value('contact@pactf.com', environ_prefix=None)
+    DEFAULT_FROM_EMAIL = values.Value('noreply@pactf.com', environ_prefix=email_prefix)
+    SERVER_EMAIL = values.Value('noreply@pactf.com', environ_prefix=None)
 
     EMAIL_BACKEND = values.Value('post_office.EmailBackend', environ_prefix=None)
     EMAIL_CRON = values.BooleanValue(False, environ_prefix=None)
@@ -440,8 +440,8 @@ class Prod(_Base):
     ''' Logging '''
 
     ADMINS = values.ListValue([
-        ('Yatharth', 'yatharth999+pactf@gmail.com'),
-        ('Tony', 'tony@tonytan.io'),
+        ('Miles', '0@rmrm.io'),
+        ('Darcy', 'dmeyer@andover.edu'),
         # ('PACTF Errors', _Django.SERVER_EMAIL.value)
     ])
 
