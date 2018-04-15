8cfb27215b765f268c46146da3f418174eaa3600
Miles McCain <milesmcc@users.noreply.github.com>
2018-04-15 01:50:25 -0400

    fix dynnamic caesar problem

--- ctfproblems/Lovelace/10_dynamic_caesar/grader.py
+++ ctfproblems/Lovelace/10_dynamic_caesar/grader.py
@@ -1,7 +1,5 @@
-import generator
-
 def grade(key, submission):
-    if submission == generator.caesar("it_is_only_uphill_from_here_%s" % str(key)):
+    if submission == "it_is_only_uphill_from_here_%s" % str(key):
         return True, "Good luck!"
     else:
         return False, "Ask Julius again..."
