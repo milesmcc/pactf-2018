3eaff43d03fb88eec03cd5dcb997b4758c6773c5
Miles McCain <milesmcc@users.noreply.github.com>
2018-04-18 20:11:46 -0400

    Make hint less obvious

--- ctfproblems/Hopper/75_library_of_babel/grader.py
+++ ctfproblems/Hopper/75_library_of_babel/grader.py
@@ -2,4 +2,4 @@ def grade(key, submission):
     if submission == 'averyunobviouskey':
         return True, "Congrats!"
     else:
-        return False, "It is a very unobvious key..."
+        return False, "You'll know it when you see it..."
