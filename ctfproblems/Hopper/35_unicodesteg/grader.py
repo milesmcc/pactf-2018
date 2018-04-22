460e1d2e4676bb71ce17a97b7c372d4269721640
Miles McCain <milesmcc@users.noreply.github.com>
2018-04-22 16:52:22 -0400

    update unicodesteg problem

--- ctfproblems/Hopper/35_unicodesteg/grader.py
+++ ctfproblems/Hopper/35_unicodesteg/grader.py
@@ -1,5 +1,5 @@
 def grade(key, submission):
-    if submission == "in plain sight":
+    if submission == "in plain sight" or submission == "the flag is in plain sight":
         return True, "Correct! Sometimes the most useful information is invisible!"
     else:
         return False, "Not quite! Sometimes looks can be deceiving..."
