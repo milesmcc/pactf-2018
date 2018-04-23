3d4cc485198d4438758e51d3402100d652a395c4
Miles McCain <milesmcc@users.noreply.github.com>
2018-04-22 20:57:07 -0400

    new steg flag

--- ctfproblems/Hopper/100_skywriting/grader.py
+++ ctfproblems/Hopper/100_skywriting/grader.py
@@ -1,5 +1,5 @@
 def grade(key, submission):
-    if submission == "a_cloud_is_just_someone_else’s_computer":
+    if submission == "a_cloud_is_just_someone_elses_computer":
         return True, "Exemplary! You should be on cloud nine!"
     else:
         return False, "Nope, that's not it! I think your head's in the clouds!"
