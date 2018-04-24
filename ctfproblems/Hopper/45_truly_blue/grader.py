3a79ae49acdb852721ed759a4a60f8a4e3e98b95
Miles McCain <milesmcc@users.noreply.github.com>
2018-04-24 18:11:16 -0400

    Add additional flag submit option for truly blue

--- ctfproblems/Hopper/45_truly_blue/grader.py
+++ ctfproblems/Hopper/45_truly_blue/grader.py
@@ -1,5 +1,5 @@
 def grade(key, submission):
-    if submission == 'last bits matter':
+    if submission == 'last bits matter' or submission == 'last bitsmatter':
         return True, "Nice job! You found the needle in the haystack!"
     else:
         return False, "Sorry, that isn't right! Looks can be deceiving..."
