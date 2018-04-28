ce2eaf2d1f6d8678836bb8672cf5e686a5d5e303
Miles McCain <milesmcc@users.noreply.github.com>
2018-04-28 12:04:39 -0400

    Add another Victor key

--- ctfproblems/Hopper/50_victor/grader.py
+++ ctfproblems/Hopper/50_victor/grader.py
@@ -1,5 +1,5 @@
 def grade(key, submission):
-    if submission == 'сделайте_это_вручную':
+    if submission == 'сделайте_это_вручную' or submission == 'СДЕЛАЙТЕ_ЭТО_ВРУЧНУЮ':
         return True, "Отличная работа. Correct!"
     else:
         return False, "Будьте лучше. Try again."
